4ea0a018a9913dae4a56cf44f41fb871a0224cfa
Igor <igorbroch11@gmail.com>
2026-01-15 21:30:42 +0000

    add `database.js` and `.env` files

---
+++ infra/database.js
@@ -0,0 +1,26 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const config = {
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  };
+
+  const client = new Client(config);
+
+  try {
+    await client.connect();
+    console.log("Connected successfully!");
+    const result = await client.query(queryObject);
+    await client.end();
+    return result;
+  } catch (error) {
+    console.error("Connection failed:", error.message);
+    throw error;
+  }
+}
+
+export default { query };
