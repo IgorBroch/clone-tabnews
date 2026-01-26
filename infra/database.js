17fc785cf3e9b0ed5fd8d22b60c84af0fbb48243
Igor <igorbroch11@gmail.com>
2026-01-26 19:51:53 +0000

    makes   more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,24 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const config = {
+  const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: Number(process.env.POSTGRES_PORT),
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  };
-
-  const client = new Client(config);
+  });
+  console.log("Credencias do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
     await client.connect();
-    console.log("Connected successfully!");
     const result = await client.query(queryObject);
-
     return result;
   } catch (error) {
     console.error("Connection failed:", error.message);
